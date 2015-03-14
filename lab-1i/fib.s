b31ebd88963ddc0f5a1119ba9d32cc898d86e4a9
Teng104 <ca9479@gmail.com>
2015-03-14 21:50:02 +0800

    Remove the unused register.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -13,15 +13,14 @@ fibonacci:
 	@ PROLOG
 	push {r3, r4, r5, r6, lr}
 
-	mov r3,#-1
-	mov r4,#1
-        mov r5,#0
-        add r6,r0,#1
+	mov r4,#-1
+	mov r5,#1
+	add r6,r0,#1
 
 for:
-	add r0,r4,r3
-        mov r3,r4
-	mov r4,r0
+	add r0,r5,r4
+        mov r4,r5
+	mov r5,r0
         
         subs r6,r6,#1
         bne for
