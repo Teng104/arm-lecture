1bf8cf8044720de91b48f4f252d514bb54a3fd06
Teng104 <ca9479@gmail.com>
2015-03-14 20:44:05 +0800

    Use assembly language to implement Fibonacci sequnece.
    Lab-1 is recursive and lab-1i is non-recursive.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,24 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r3,#-1
+	mov r4,#1
+        mov r5,#0
+        add r6,r0,#1
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+for:
+	add r0,r4,r3
+        mov r3,r4
+	mov r4,r0
+        
+        subs r6,r6,#1
+        bne for
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r3, r4, r5, r6, pc}	@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
